53d42ba431d27bf130715205f0d6f94a0ec7d4cd
Flavio Troia <flavio.troia@assistdigital.com>
2023-10-31 17:29:23 +0100

    fix: add invoke

--- main.go
+++ main.go
@@ -1,45 +1,79 @@
 package main
+
 import (
-    "bytes"
-    "log"
-    "net/http"
-    "github.com/gin-gonic/gin"
-    "github.com/gin-contrib/cors"
+	"bytes"
+	"encoding/json"
+	"io"
+	"log"
+	"net/http"
+
+	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/gin"
+	"github.com/pkg/errors"
 )
 
+func Invoke[T any](url string, method string, payload any) (resp *T, err error) {
+	body, err := json.Marshal(payload)
+	if err != nil {
+		return nil, err
+	}
+	req, err := http.NewRequest(method, url, bytes.NewReader(body))
+	if err != nil {
+		return nil, err
+	}
+
+	client := http.Client{}
+	res, err := client.Do(req)
+	log.Println(res)
+
+	if err != nil {
+		return nil, errors.Wrap(err, "making request to endpoint")
+	}
+
+	resBody, err := io.ReadAll(res.Body)
+	if err != nil {
+		return nil, errors.Wrap(err, "reading ADB response body")
+	}
+
+	err = json.Unmarshal(resBody, &resp)
+	if err != nil {
+		return nil, errors.Wrap(err, "decoding JSON response body")
+	}
+
+	if res.StatusCode != http.StatusOK {
+		return nil, errors.Errorf("response not OK from ADB, with body: %+v", resp)
+	}
+
+	return resp, nil
+}
+
 type AddParams struct {
-    Url string `json:"url" binding:"required,gte=1"`
-    Method string `json:"method"`
-    Data string `json:"data"`
+	Url     string `json:"url" binding:"required,gte=1"`
+	Method  string `json:"method"`
+	Payload string `json:"payload"`
 }
 
 func call(c *gin.Context) {
-    var ap AddParams
-    if err := c.ShouldBindJSON(&ap); err != nil {
-        c.JSON(400, gin.H{"error": "Invalid input!"})
-        return
-    }
+	var ap AddParams
+	if err := c.ShouldBindJSON(&ap); err != nil {
+		c.JSON(400, gin.H{"error": "Invalid input!"})
+		return
+	}
 
-    // TODO: add fetch
-    jsonBody := []byte(ap.Data)
-    bodyReader := bytes.NewReader(jsonBody)
-    res, err := http.NewRequest(ap.Method, ap.Url, bodyReader)
+	res, err := Invoke[string](ap.Url, ap.Method, ap.Payload)
+	if err != nil {
+		c.JSON(400, gin.H{"error": err})
+	}
 
-    log.Printf("url: %a, method: %b, data: %c", ap.Url, ap.Method, ap.Data)
+	log.Println("Response from endpoint: " + *res)
 
-    if err != nil {
-        c.JSON(400, err)
-        return
-    }
-
-    c.JSON(200,  gin.H{"response": res})
+	c.JSON(200, gin.H{"response": *res})
 }
 
-
 func main() {
-    router := gin.Default()
-    router.Use(cors.Default())
-    router.POST("/", call)
+	router := gin.Default()
+	router.Use(cors.Default())
+	router.POST("/", call)
 
-    router.Run(":5000")
+	router.Run(":5000")
 }
